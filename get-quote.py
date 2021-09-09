ec65c66b86cfd62c77ca8bb49474a6f6582bf1c0
saurishc <saurishc@gmail.com>
2021-09-09 16:56:38 +0530

    random quote

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
 def xain():
    print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+   f = open("quotes.txt")
+   quotes = f.readlines()
+   f.close()
 
-  #print(quotes)
+   from random import randint
+   print(quotes[randint(0,len(quotes)-1)])
 
 if __name__== "__main__":
   xain()
